384050c5f5d6b68bb4b87f8469fa37cba757a185
vikas kumar <vikas10145.12@bitmesra.ac.in>
2024-09-27 20:14:57 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Navbar from './Navbar';
+import Card from './Card';
+
+
+
+function App() {
+  return (
+    <div className="App">
+        <Navbar/>
+        <div className='body-container'>
+        <Card/>
+        </div>
+        
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Card.jsx
@@ -0,0 +1,53 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useParams } from "react-router-dom";
+import './Card.css';
+
+// const Fetch = () => {
+//     const [news, setNews] = useState([]);
+//     useEffect(() => {
+//       fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=c6d0646c9ad54fb880cf03351dc617cd')
+//         .then((res) => {
+//           return res.json();
+//         })
+//         .then((data) => {   
+//           console.log(data);
+//           setNews(data);
+//         });
+//     }, []);
+// }
+
+
+// const card = {
+//     img :"im1",
+//     title: "title 1",
+//     text: "Some quick example text to build on the card title and make up the bulk of the card's content."
+// }
+function Card() {
+    let params = useParams();
+    const [news, setNews] = useState(null);
+    useEffect(() => { 
+      axios.get('https://newsapi.org/v2/top-headlines?country=us&apiKey=c6d0646c9ad54fb880cf03351dc617cd')
+        .then(response => {
+                setNews(response.data.articles);
+                console.log(news);
+              });
+    }, [])
+  return (
+    <div className="Card-container">
+      {
+      !news?null:news.map( (data) => (
+        <div className="card">  
+          <img src={data.urlToImage} className="card-img-top" alt="..."/>
+            <div className="card-body">
+                <h5 className="card-title">{data.title}</h5>
+                <p className="card-text">{data.content}</p>
+                <a href={data.url} className="btn btn-primary">Details</a>
+            </div>
+        </div>
+        ))
+      }
+    </div>
+  );
+ }
+export default Card;

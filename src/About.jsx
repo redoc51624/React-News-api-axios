7e4c2b55fc6bb2524e255e5c5432b3fc389161c2
vikas kumar <vikas10145.12@bitmesra.ac.in>
2024-10-18 20:38:16 +0530

    -Babel changes for build netlify
    --Babel changes for build netlify

--- src/About.jsx
+++ src/About.jsx
@@ -2,9 +2,19 @@ import React from 'react';
 
 const About = () => {
   return (
-    <div>
-      <h2>About us component</h2>
-      
+    <div className='container'>
+      <h2>About us</h2>
+      Welcome to Vikas News, your trusted source for breaking news, in-depth analysis, and diverse perspectives on the stories that matter most. Founded in 2024, we are committed to delivering accurate, unbiased reporting to keep you informed and engaged.
+
+At Vikas News, we believe that a well-informed public is essential for a thriving democracy. Our team of experienced journalists and analysts works tirelessly to bring you the latest developments across a range of topics, including politics, business, technology, culture, and more. We prioritize integrity and transparency, ensuring our reporting is both reliable and insightful.
+
+We understand the importance of context in today’s fast-paced news cycle. That’s why we not only cover the headlines but also provide the background and analysis needed to understand the implications of each story. Whether you’re looking for breaking news updates, feature articles, or opinion pieces, we strive to offer something for everyone.
+
+Join us as we navigate the complexities of the world together. We encourage our readers to engage with us, share your thoughts, and become part of our growing community. Thank you for choosing Vikas News as your go-to news source!
+
+
+
+
     </div>
   )
 }
--- src/Home.jsx
+++ src/Home.jsx
@@ -5,10 +5,11 @@ const Home = () => {
     
     // const categories = ["business","entertainment","health","science","sports","technology"];
     const apiKey = process.env.REACT_APP_NEWS_API;
-
+    const category = "general";
     return (
     <div>
-      <Card apiKey={apiKey}/>
+      <h2>All top-headlines</h2>
+      <Card key={category} apiKey={apiKey} category={category}>about card</Card>
     </div>
   )
 }
